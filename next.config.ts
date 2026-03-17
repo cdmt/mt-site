75de73220a6a138ce9227d9f0b976e2bd555937d
chris dickinson <chris@moretype.co.uk>
2026-03-17 16:29:12 +0000

    add logo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "moretype.fontdue.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,46 @@ import "./globals.css";
 import "fontdue-js/fontdue.css";
 import FontdueProvider from "fontdue-js/FontdueProvider";
 import StoreModal from "fontdue-js/StoreModal";
+import { fetchGraphql } from "@/lib/graphql";
+import { RootLayoutQuery } from "../../operations-types";
+import Image from "next/image";
+
 
 const fontdueUrl = process.env.NEXT_PUBLIC_FONTDUE_URL;
 
+
+async function getData() {
+  return fetchGraphql<RootLayoutQuery>("RootLayout.graphql");
+}
 
 export const metadata: Metadata = {
     title: "Moretype",
     description: "Moretype type foundry",
 };
 
-    export default function RootLayout({
+export default async function RootLayout({
         children,
     }: Readonly<{
         children: React.ReactNode;
     }>) {
+
+    const { viewer } = await getData();
+
+    const pages = viewer.pages?.edges?.map((edge) => edge!.node!);   
+
     return (
         <html lang="en">
             <body>
-                
                 <FontdueProvider url={fontdueUrl}>
+                    <nav>
+                        <Image
+                            src={viewer?.logo?.url!}
+                            alt="Logo"
+                            width={(viewer?.logo?.meta.width ?? 100) / 2}
+                            height={(viewer?.logo?.meta.height ?? 100) / 2}
+                            priority
+                        />
+                    </nav>
                     {children}
                     <StoreModal />
                 </FontdueProvider>
