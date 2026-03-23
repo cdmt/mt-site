02718223a6977ba58c19cac60e9306143ca98b26
chris dickinson <chris@moretype.co.uk>
2026-03-23 09:27:26 +0000

    footer add fonts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,33 @@
 import globals from "@/app/styles/global.module.css";
 import { fetchGraphql } from "@/lib/graphql";
 import { FooterQuery } from "../../operations-types";
+import Link from "next/link";
+import footer_styles from '../app/styles/footer.module.css'
 
 async function getData() {
-  return fetchGraphql<FooterQuery>("Footer.graphql");
+    return fetchGraphql<FooterQuery>("Footer.graphql");
 }
 
-export default async function Footer() {
-  const { viewer } = await getData();
+const { viewer } = await getData();
+const fonts =
+    viewer?.fontCollections?.edges?.flatMap(edge =>
+        edge?.node ? [edge.node] : [],
+    ) ?? [];
 
-  return (
-    <footer className={globals.footer}>
-      <p>{viewer?.settings?.footerText}</p>
-    </footer>
-  );
+export default async function Footer() {
+
+    return (
+        <footer className={globals.footer}>
+            <div className={footer_styles.footer_wrap}>
+                <div className={footer_styles.footer_fonts}>
+                    {fonts.map((font, index) => (
+                        <Link href={font.slug?.name ? `/fonts/${font.slug.name}` : "/fonts"} key={font.slug?.name}>
+                            {font.name} •{' '}
+                        </Link>
+                    ))}
+                </div>
+                <p>{viewer?.settings?.footerText}</p>
+            </div>
+        </footer>
+    );
 }
