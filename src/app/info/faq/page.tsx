1d00e6ee2ea8de05fb39df185b47d3d1e69c360a
chris dickinson <chris@moretype.co.uk>
2026-04-07 18:32:18 +0100

    info check spelling

--- src/app/info/faq/page.tsx
+++ src/app/info/faq/page.tsx
@@ -5,24 +5,32 @@ export default function FAQ() {
     return(
         <div className={global_styles.info_page}>
             <dl className={info_styles.faq_page}>
-                <dt><h4>What format are the fonts?</h4></dt>
+                <dt><h4>What formats are the fonts available in?</h4></dt>
                 <dd>The fonts are provided in OpenType, WOFF, and WOFF2 formats. Additional formats may be available upon request.</dd>
-                <dt><h4>What is Opentype?</h4></dt>
-                <dd>The Opentype font format is a cross-platform extension of the TrueType font format developed by jointly by Adobe and Microsoft. Opentype fonts can contain extended character sets and advanced layout features for use in Opentype savvy applications.</dd>
+
+                <dt><h4>What is OpenType?</h4></dt>
+                <dd>The OpenType font format is a cross-platform extension of the TrueType font format, developed jointly by Adobe and Microsoft. OpenType fonts can contain extended character sets and advanced layout features for use in OpenType-savvy applications.</dd>
+
                 <dt><h4>What is an End User Licence Agreement?</h4></dt>
-                <dd>An EULA is a legal contract between the manufacturer and/or the author and the end user of an application. The EULA details how the software can and cannot be used and any restrictions that the manufacturer imposes (e.g., most EULA’s of proprietary software prohibit the user from sharing the software with anyone else).The Moretype EULA sets out the limitations of the use of the font, you have to agree to have read and understood this before you can purchase the fonts.</dd>
+                <dd>An EULA is a legal contract between the manufacturer and/or the author and the end user of an application. The EULA details how the software can and cannot be used, as well as any restrictions imposed by the manufacturer (e.g., most EULAs for proprietary software prohibit users from sharing the software with others). The Moretype EULA sets out the limitations on the use of the font, and you must confirm that you have read and understood it before purchasing the fonts.</dd>
+
                 <dt><h4>What languages do the fonts support?</h4></dt>
                 <dd>The supported character sets are shown on the individual type pages.</dd>
+
                 <dt><h4>Why are some fonts named “New”?</h4></dt>
-                <dd>The fonts were originally released in a non OpenType format. When the fonts were upgraded to OpenType we took the opportunity to re-drawer, re-space and re-kern the fonts. The degree of changes warranted the fonts to be renamed as “New”.</dd>
+                <dd>The fonts were originally released in a non-OpenType format. When the fonts were upgraded to OpenType, we took the opportunity to redraw, respace, and re-kern them. The extent of these changes warranted renaming the fonts as “New”.</dd>
+
                 <dt><h4>Can the fonts be returned?</h4></dt>
-                <dd>Due to the fonts being digital products they cannot be returned for a refund. We go to every effort during the design and testing process to ensure the fonts are produced to the highest technical standards. If you have any issue please contact us directly.</dd>
-                <dt><h4>Can I alter, add or change the fonts?</h4></dt>
+                <dd>As the fonts are digital products, they cannot be returned for a refund. We make every effort during the design and testing process to ensure the fonts meet the highest technical standards. If you experience any issues, please contact us directly.</dd>
+
+                <dt><h4>Can I alter, add to, or change the fonts?</h4></dt>
                 <dd>Fonts must be used as provided and may not be altered, edited, or modified. This includes all glyphs, which cannot be changed for logo design, branding, or any other use.</dd>
-                <dt><h4>Can I use the fonts in a website?</h4></dt>
-                <dd>Fonts can be used in a website providing they are in a secure, read only mode. This is covered in more detail in the Moretype EULA. Moretype fonts are available as web fonts from Typekit, Fontdeck and Webink</dd>
-                <dt><h4>Have another question ?</h4></dt>
-                <dd>If you have another question you get in touch <a href="mailto:info@moretype.co.uk" className={info_styles.faq_link}>info@moretype.co.uk</a></dd>
+
+                <dt><h4>Can I use the fonts on a website?</h4></dt>
+                <dd>Fonts can be used on a website provided they are in a secure, read-only mode. This is covered in more detail in the Moretype EULA. Moretype fonts are available as web fonts from Typekit, Fontdeck, and WebINK.</dd>
+
+                <dt><h4>Have another question?</h4></dt>
+                <dd>If you have another question, please get in touch <a href="mailto:info@moretype.co.uk" className={info_styles.faq_link}>info@moretype.co.uk</a></dd>
             </dl>
         </div>
     )
--- src/app/info/installing-fonts/page.tsx
+++ src/app/info/installing-fonts/page.tsx
@@ -6,7 +6,7 @@ export default function InstallFonts() {
     return(
         <div className={`${info_styles.eula} ${global_styles.info_page}`}>
             <section>
-                <h3>Whether you are installing fonts on a Mac or PC you should first.</h3>
+                <h3>Whether you are installing fonts on a Mac or PC, you should first:</h3>
                 <ul>
                     <li>
                         Close all open applications.
@@ -15,39 +15,46 @@ export default function InstallFonts() {
                         Unzip the downloaded file containing the fonts.
                     </li>
                     <li>
-                        On your desktop you should have a folder for each font weight or font family you wish to install.
+                        On your desktop, you should have a folder for each font weight or font family you wish to install.
                     </li>
                 </ul>
             </section>
+
             <section>
                 <h3>Install Fonts on a Mac</h3>
-                <h4>Add files to the Fonts Folder</h4>
+                <h4>Add files to the Fonts folder</h4>
                 <ul>
                     <li>Drag the font files from your desktop to your account Fonts folder.</li>
-                    <li>The account fonts folder is at Username {">"} Library {">"} Fonts.</li>
-                    <li>These font will then be available to your account only.</li>
-                    <li>Alternatively you can add the fonts to the Macintosh HD Fonts folder.</li>
-                    <li>The HD Font folder is at Macintosh HD {">"} Library {">"} Fonts.</li>
-                    <li>The font will then be available to all users.,</li>
+                    <li>The account Fonts folder is located at Username {">"} Library {">"} Fonts.</li>
+                    <li>These fonts will then be available to your account only.</li>
+                    <li>Alternatively, you can add the fonts to the Macintosh HD Fonts folder.</li>
+                    <li>The HD Fonts folder is located at Macintosh HD {">"} Library {">"} Fonts.</li>
+                    <li>The fonts will then be available to all users.</li>
                 </ul>
-                <h4>Add Fonts with Font Book</h4>
+
+                <h4>Add fonts with Font Book</h4>
                 <ul>
-                    <li>Font Book is free with Mac OSX.</li>
-                    <li>Simple double click the font files on your desktop.</li>
-                    <li>Font Book open and add the fonts to your fonts folder.</li>
+                    <li>Font Book is included with macOS.</li>
+                    <li>Simply double-click the font files on your desktop.</li>
+                    <li>Font Book will open and add the fonts to your Fonts folder.</li>
                 </ul>
-                Further information about installing fonts on a Mac can be found <Link href="https://support.apple.com/en-gb/guide/font-book/fntbk1000/mac" target="_blank" className={info_styles.install_link}>here</Link>
+
+                Further information about installing fonts on a Mac can be found{" "}
+                <Link href="https://support.apple.com/en-gb/guide/font-book/fntbk1000/mac" target="_blank" className={info_styles.install_link}>here</Link>
             </section>
+
             <section>
                 <h3>Install Fonts on a PC</h3>
                 <ul>
                     <li>Unpack the zip folder that you downloaded.</li>
-                    <li>Go to the folder with the fonts and search for the .OTF or .TTF file (we recommend you always install the OTF file if available).</li>
-                    <li>Double-click on the font as if you are opening an application.</li>
-                    <li>Now the font installation window will open.</li>
-                    <li>Click Install and wait till it’s done.</li>
+                    <li>Go to the folder with the fonts and locate the .OTF or .TTF file (we recommend installing the OTF file if available).</li>
+                    <li>Double-click the font file as if you are opening an application.</li>
+                    <li>The font installation window will open.</li>
+                    <li>Click Install and wait until it is complete.</li>
                 </ul>
-                Further information about installing fonts on a Mac can be found <Link href="https://support.microsoft.com/en-us/office/add-a-font-b7c5f17c-4426-4b53-967f-455339c564c1" target="_blank" className={info_styles.install_link}>here</Link>
+
+                Further information about installing fonts on a PC can be found{" "}
+                <Link href="https://support.microsoft.com/en-us/office/add-a-font-b7c5f17c-4426-4b53-967f-455339c564c1" target="_blank" className={info_styles.install_link}>here</Link>
             </section>
         </div>
     )
