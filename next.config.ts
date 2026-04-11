e55dddac76954edd72a488f96e470ef1306a4e9d
chris dickinson <chris@moretype.co.uk>
2026-04-11 13:04:15 +0100

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.fontdue.com",
       },
+      {
+        protocol: "https",
+        hostname: "store.moretype.co.uk",
+      },
     ],
   },
 };
